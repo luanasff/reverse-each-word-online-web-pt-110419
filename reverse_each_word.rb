678b9989f75a5e86993d65449cbf81ff1d57cc0e
Luana Faria Lane <luanasfff@gmail.com>
2019-11-14 02:33:32 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
-
 def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
- end
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
 
